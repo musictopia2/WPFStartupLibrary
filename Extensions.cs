f500dc2adddd93748094b6f2b0216fd80d08f6a0
Andy Walter <musictopia2@hotmail.com>
2025-12-03 18:22:09 -0700

    Converted to using the new extension model in c#.

--- Extensions.cs
+++ Extensions.cs
@@ -1,29 +1,35 @@
 ﻿namespace WPFStartupLibrary;
 public static class Extensions
 {
-    public static void RegisterWPFServices(this IServiceCollection services)
+    extension (IServiceCollection services)
     {
-        CustomWindowsClasses windows = new();
-        //not important that its a different reference.
-        BasicBlazorLibrary.Helpers.BlazorUIHelpers.MessageBox = windows;
-        BasicBlazorLibrary.Helpers.BlazorUIHelpers.SystemError = windows;
-        BasicBlazorLibrary.Helpers.BlazorUIHelpers.Exit = windows;
-        services.AddSingleton<CustomWindowsClasses>();
-        services.AddSingleton<ILayout>(xx => xx.GetRequiredService<CustomWindowsClasses>());
-        services.AddSingleton<IExit>(xx => xx.GetRequiredService<CustomWindowsClasses>());
-        services.AddSingleton<IMessageBox>(xx => xx.GetRequiredService<CustomWindowsClasses>());
-        services.AddSingleton<ISystemError>(xx => xx.GetRequiredService<CustomWindowsClasses>());
-        services.AddSingleton<IWindowStateManager>(xx => xx.GetRequiredService<CustomWindowsClasses>());
-        aa1.OS = aa1.EnumOS.WindowsDT;
-        UIPlatform.DesktopValidationError = (message) => MessageBox.Show(message);
-        UIPlatform.CurrentThread = new WPFThread();
-    }
-    public static void RegisterFullScreen(this Window window)
-    {
-        mm1.ProcessFullScreen = () =>
+        public void RegisterWPFServices()
         {
-            window.WindowStyle = WindowStyle.None;
-            window.WindowState = WindowState.Maximized;
-        };
+            CustomWindowsClasses windows = new();
+            //not important that its a different reference.
+            BasicBlazorLibrary.Helpers.BlazorUIHelpers.MessageBox = windows;
+            BasicBlazorLibrary.Helpers.BlazorUIHelpers.SystemError = windows;
+            BasicBlazorLibrary.Helpers.BlazorUIHelpers.Exit = windows;
+            services.AddSingleton<CustomWindowsClasses>();
+            services.AddSingleton<ILayout>(xx => xx.GetRequiredService<CustomWindowsClasses>());
+            services.AddSingleton<IExit>(xx => xx.GetRequiredService<CustomWindowsClasses>());
+            services.AddSingleton<IMessageBox>(xx => xx.GetRequiredService<CustomWindowsClasses>());
+            services.AddSingleton<ISystemError>(xx => xx.GetRequiredService<CustomWindowsClasses>());
+            services.AddSingleton<IWindowStateManager>(xx => xx.GetRequiredService<CustomWindowsClasses>());
+            aa1.OS = aa1.EnumOS.WindowsDT;
+            UIPlatform.DesktopValidationError = (message) => MessageBox.Show(message);
+            UIPlatform.CurrentThread = new WPFThread();
+        }
+    }
+    extension(Window window)
+    {
+        public void RegisterFullScreen()
+        {
+            mm1.ProcessFullScreen = () =>
+            {
+                window.WindowStyle = WindowStyle.None;
+                window.WindowState = WindowState.Maximized;
+            };
+        }
     }
 }
